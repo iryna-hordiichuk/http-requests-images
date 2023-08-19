58013ddf233713611099bbaf908b58882d83158b
Iryna Hordiichuk <72663259+IrynaHordiichuk@users.noreply.github.com>
2023-08-19 19:18:37 +0100

    adds logic for http requests, task1

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,30 @@ import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
+  state = {
+    query: '',
+  };
+
+  onChange = (event) => {
+  const value = event.target.value;
+this.setState({query: value})
+
+  };
+  handleSubmit = (event) => {
+event.preventDefault ();
+console.log('hello world');
+this.props.onSubmit(this.state.query);
+this.setState({query: ''});
+  };
+
   render() {
-    return <h2>SearchForm</h2>;
+    return (
+      <>
+        <SearchFormStyled onSubmit={this.handleSubmit}>
+        <InputSearch onChange={this.onChange}  value={this.state.query}/>
+       <FormBtn><FiSearch/></FormBtn>
+        </SearchFormStyled>
+      </>
+    );
   }
-}
+};
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -3,11 +3,82 @@ import { Component } from 'react';
 import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
+const IMAGES_PER_PAGE = 15;
+
 export class Gallery extends Component {
+  state = {
+    images: [],
+    query: '',
+    page: 1,
+    isLoading: false,
+    isShown: false,
+    isEmpty: false,
+  };
+
+  componentDidUpdate(_, prevState) {
+    const { query, page } = this.state;
+    if (prevState.query !== query || prevState.page !== page) {
+      this.getImagesGallery(query, page);
+    }
+  }
+
+  onSubmit = value => {
+    this.setState({
+      query: value,
+      page: 1,
+      images: [],
+      isShown: false,
+      isEmpty: false,
+    });
+  };
+
+  getImagesGallery = async (query, page) => {
+    if (!query) {
+      return;
+    }
+
+    this.setState({ isLoading: true });
+    const images = await ImageService.getImages(query, page);
+    console.log(images);
+    if (images.photos.length === 0){
+      this.setState({isEmpty: true});
+    }
+    this.setState(prevState => ({
+      images: [...prevState.images, ...images.photos],
+      isShown:
+        prevState.page < Math.ceil(images.total_results / IMAGES_PER_PAGE),
+      isLoading: false,
+    }));
+  };
+
+  getPage = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
   render() {
+    const { isEmpty, images, isLoading } = this.state;
     return (
       <>
-        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        <SearchForm onSubmit={this.onSubmit} />
+        <Grid>
+          {images.map(image => (
+            <GridItem key={image.id}>
+              <CardItem color={image.avg_color}>
+                <img src={image.src.large} alt={image.alt} />
+              </CardItem>
+            </GridItem>
+          ))}
+        </Grid>
+
+        {this.state.isShown && (
+          <Button onClick={this.getPage}>
+            {isLoading ? 'Loading ...' : 'Load More'}
+          </Button>
+        )}
+
+        { isEmpty && (
+          <Text textAlign="center">Sorry. There are no images ... 😭🍕</Text>
+        )}
       </>
     );
   }
